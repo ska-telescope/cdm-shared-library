411fcad6bcf21a4d09ed3dc0a51625754871917a
HFRibeiro <ribeirh02@gmail.com>
2019-06-17 09:25:15 +0100

    fixed version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as readme_file:
 
 setup(
     name='cdm-shared-library',
-    version='0.1.0',
+    version='0.0.9',
     description="Configuration data model library",
     long_description=readme + '\n\n',
     author="Hélder Ribeiro",
