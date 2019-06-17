d1fa6cf77dd35c8fcbac8ceb72e2404046b51449
HFRibeiro <ribeirh02@gmail.com>
2019-06-17 09:37:32 +0100

    add artifacts to CI

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as readme_file:
 
 setup(
     name='cdm-shared-library',
-    version='0.0.9',
+    version='0.0.10',
     description="Configuration data model library",
     long_description=readme + '\n\n',
     author="Hélder Ribeiro",
