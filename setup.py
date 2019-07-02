c791920dc9a8127d49bb3b49b567e4efe99a90cd
Mark Nicol <mark.nicol@stfc.ac.uk>
2019-07-02 11:27:32 +0100

    bumped to version 0.1.9

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as readme_file:
 
 setup(
     name='cdm-shared-library',
-    version="0.1.8",
+    version="0.1.9",
     description="Configuration data model library",
     long_description=readme + '\n\n',
     author="Hélder Ribeiro",
