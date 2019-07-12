5a190ba2aab5b62213afedade94e078fbbc76d73
Snehal Valame <snehal_valame@persistent.com>
2019-07-12 19:42:23 +0530

    add placeholder for configure subarray tests

---
+++ tests/ska/cdm/messages/subarray_node/test_configure_subarray.py
@@ -0,0 +1,11 @@
+"""
+Unit tests for the SubarrayNode.Configure request/response mapper module.
+"""
+from ska.cdm.messages.subarray_node.configure_subarray import ConfigureRequest,\
+    DishConfiguration, PointingConfiguration
+
+
+# TODO
+def test_configure_subarray_request_eq():
+   assert 1==1
+
