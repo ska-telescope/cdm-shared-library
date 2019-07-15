a0bda820d76eff1ba11f0279313bd4f1406d16f6
Stewart Williams <16945415+bravostuzero@users.noreply.github.com>
2019-07-15 10:01:37 +0100

    bug fix: configure_subarray module has been renamed

--- ska/cdm/messages/subarray_node/__init__.py
+++ ska/cdm/messages/subarray_node/__init__.py
@@ -2,5 +2,5 @@
 The ska.cdm.messages.subarray_node package holds modules that translate TMC
 Subarray Node requests and responses to and from Python.
 """
-from .configure_subarray import SubarrayConfiguration, PointingConfiguration,DishConfiguration,ConfigureRequest
-from .scan_subarray import ScanRequest
+from .configure import SubarrayConfiguration, PointingConfiguration, DishConfiguration, ConfigureRequest
+from .scan import ScanRequest
