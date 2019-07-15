66076f2a20bd31073f8fb2150bee08341a61c7d6
Stewart Williams <16945415+bravostuzero@users.noreply.github.com>
2019-07-15 07:30:03 +0100

    Rename subarray_node modules to match names of Tango commands

--- ska/cdm/messages/subarray_node/scan.py
+++ ska/cdm/messages/subarray_node/scan.py
@@ -2,9 +2,6 @@
 The messages module provides simple Python representations of the structured
 request and response for the TMC SubArrayNode.Scan command.
 """
-from typing import Optional, List
-from astropy.coordinates import SkyCoord
-
 __all__ = ['ScanRequest']
 
 
