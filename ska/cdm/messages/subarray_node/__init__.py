d6934e59e583f16f848e4879fd66793fbd48d30f
Mark Nicol <mark.nicol@stfc.ac.uk>
2019-07-29 08:04:14 +0100

    Initial version of SDP Configure and Configure scan changes - before integration into mainConfigure request

--- ska/cdm/messages/subarray_node/__init__.py
+++ ska/cdm/messages/subarray_node/__init__.py
@@ -3,5 +3,5 @@ The ska.cdm.messages.subarray_node package holds modules that translate TMC
 Subarray Node requests and responses to and from Python.
 """
 from .configure import PointingConfiguration, DishConfiguration, ConfigureRequest, Target, \
-    ReceiverBand
+    ReceiverBand,  SDPConfigurationBlock, SDPConfigure, SDPConfigureScan, SDPParameters, SDPScan, SDPScanParameters, SDPWorkflow
 from .scan import ScanRequest
--- ska/cdm/messages/subarray_node/configure.py
+++ ska/cdm/messages/subarray_node/configure.py
@@ -3,12 +3,13 @@ The messages module provides simple Python representations of the structured
 request and response for the TMC SubArrayNode.Configure command.
 """
 from enum import Enum
+from typing import Dict
 
 from astropy.coordinates import SkyCoord
 
 __all__ = ['ConfigureRequest', 'DishConfiguration', 'PointingConfiguration', 'Target',
-           'ReceiverBand']
-
+           'ReceiverBand', 'SDPConfigurationBlock', 'SDPConfigureScan', 'SDPParameters',
+           'SDPScan', 'SDPConfigure', 'SDPWorkflow']
 
 class Target:
     """
@@ -17,7 +18,6 @@ class Target:
     The SubArrayNode ICD specifies that RA and Dec must be provided, hence
     non-ra/dec frames such as galactic are not supported.
     """
-
     def __init__(self, ra, dec, name='', frame='icrs', unit=('hourangle', 'deg')):
         self.coord = SkyCoord(ra, dec, unit=unit, frame=frame)
         self.name = name
@@ -107,3 +107,110 @@ class ConfigureRequest:  # pylint: disable=too-few-public-methods
         return self.pointing == other.pointing \
                and self.dish == other.dish \
                and self.scan_id == other.scan_id
+
+
+class SDPWorkflow:
+
+    def __init__(self, wf_id: str, wf_type: str, version: str):
+        self.wf_id = wf_id
+        self.wf_type = wf_type
+        self.version = version
+
+    def __eq__(self, other):
+        if not isinstance(other, SDPWorkflow):
+            return False
+        return all([self.wf_id == other.wf_id,
+                    self.wf_type == other.wf_type,
+                    self.version == other.version])
+
+
+class SDPParameters:
+
+    def __init__(self, num_stations: int, num_chanels: int, num_polarisations: int, freq_start_hz: float,
+                 freq_end_hz: float, target_fields: Dict[int, Target]):
+        self.num_stations = num_stations
+        self.num_chanels = num_chanels
+        self.num_polarisations = num_polarisations
+        self.freq_start_hz = freq_start_hz
+        self.freq_end_hz = freq_end_hz
+        self.target_fields = target_fields
+
+    def __eq__(self, other):
+        if not isinstance(other, SDPParameters):
+            return False
+        return all([self.num_stations == other.num_stations,
+                    self.num_chanels == other.num_chanels,
+                    self.num_polarisations == other.num_polarisations,
+                    self.freq_start_hz == other.freq_start_hz,
+                    self.freq_end_hz == other.freq_end_hz,
+                    self.target_fields == other.target_fields])
+
+
+class SDPScan:
+    def __init__(self, field_id: int, interval_ms: int) -> None:
+        self.field_id = field_id
+        self.interval_ms = interval_ms
+
+    def __eq__(self, other):
+        if not isinstance(other, SDPScan):
+            return False
+        return all([self.field_id == other.field_id,
+                    self.interval_ms == other.interval_ms])
+
+
+class SDPScanParameters:
+
+    def __init__(self, scan_parameters: Dict[str, SDPScan]):
+        self.scan_parameters = scan_parameters
+
+    def __eq__(self, other):
+        if not isinstance(other, SDPScanParameters):
+            return False
+        return self.scan_parameters == other.scan_parameters
+
+
+class SDPConfigurationBlock:
+
+    def __init__(self, sb_id: str, sbi_id: str, workflow: SDPWorkflow,
+                 parameters: SDPParameters, scan_parameters: Dict[str, SDPScan]):
+        """
+
+        :type scan_parameters: Dict[str, ska.cdm.messages.subarray_node.configure.SDPScan]
+        """
+        self.sb_id = sb_id
+        self.sbi_id = sbi_id
+        self.workflow = workflow
+        self.parameters = parameters
+        self.scan_parameters = scan_parameters
+
+    def __eq__(self, other):
+        if not isinstance(other, SDPConfigurationBlock):
+            return False
+        return all([self.sb_id == other.sb_id,
+                    self.sbi_id == other.sbi_id,
+                    self.workflow == other.workflow,
+                    self.parameters == other.parameters,
+                    self.scan_parameters == other.scan_parameters])
+
+
+class SDPConfigure:
+
+    def __init__(self, configure: [SDPConfigurationBlock]):
+        self.configure = configure
+
+    def __eq__(self, other):
+        if not isinstance(other, SDPConfigure):
+            return False
+        return self.configure == other.configure
+
+
+class SDPConfigureScan:
+
+    def __init__(self, configure_scan: SDPScanParameters):
+        self.configure_scan = configure_scan
+
+    def __eq__(self, other):
+        if not isinstance(other, SDPConfigureScan):
+            return False
+        return self.configure_scan == other.configure_scan
+
