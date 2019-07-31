0ba0f23669e8d9728eb3eb8df436a22d231f8e2c
Mark Nicol <mark.nicol@stfc.ac.uk>
2019-07-31 08:15:54 +0100

    Fix issues highlighted by full linting - formatting and 1 code difference from Python 3.5

--- ska/cdm/messages/subarray_node/__init__.py
+++ ska/cdm/messages/subarray_node/__init__.py
@@ -3,5 +3,6 @@ The ska.cdm.messages.subarray_node package holds modules that translate TMC
 Subarray Node requests and responses to and from Python.
 """
 from .configure import PointingConfiguration, DishConfiguration, ConfigureRequest, Target, \
-    ReceiverBand,  SDPConfigurationBlock, SDPConfigure, SDPConfigureScan, SDPParameters, SDPScan, SDPScanParameters, SDPWorkflow
+    ReceiverBand, SDPConfigurationBlock, SDPConfigure, SDPConfigureScan, SDPParameters, \
+    SDPScan, SDPScanParameters, SDPWorkflow
 from .scan import ScanRequest
