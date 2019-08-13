43a20e5ae51546ae45e3bdaa598ea7bab6fdb595
Stewart Williams <16945415+bravostuzero@users.noreply.github.com>
2019-08-13 13:34:36 +0100

    bumped to version 0.1.28

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as readme_file:
 
 setup(
     name='cdm-shared-library',
-    version="0.1.27",
+    version="0.1.28",
     description="Configuration data model library",
     long_description=readme + '\n\n',
     author="Hélder Ribeiro",
