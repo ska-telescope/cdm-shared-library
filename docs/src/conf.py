80d12be687f9b720f05db40a085c2d3fea9a3d69
handofkwll <jfl@roe.ac.uk>
2020-09-28 09:41:27 +0100

    added mock modules to get redthedocs working

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -22,7 +22,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath('../../src'))
-
+autodoc_mock_imports = ['astropy', 'marshmallow']
 
 def setup(app):
     app.add_stylesheet('css/custom.css')
