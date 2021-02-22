cdc9e41662166dae61295cba195fb6b440fd2f81
abhijeet deolalikar <abhijeet_deolalikar@persistent.com>
2021-02-22 10:41:41 +0000

    bumped to version 3.0.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open('README.md') as readme_file:
 
 setuptools.setup(
     name='cdm-shared-library',
-    version="2.1.1",
+    version="3.0.0",
     description="Configuration data model library",
     long_description=readme + '\n\n',
     author="Hélder Ribeiro",
