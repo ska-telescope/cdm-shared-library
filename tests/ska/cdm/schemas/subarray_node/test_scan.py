9441e2bf19cef99fe35b0b7cbc2c0fc3a7927a04
Abhijeet Deolalikar <abhijeet_deolalikar@persistent.com>
2021-04-06 14:01:18 +0100

    modify tests for scan to support oso-tmc low schema json

--- tests/ska/cdm/schemas/subarray_node/test_scan.py
+++ tests/ska/cdm/schemas/subarray_node/test_scan.py
@@ -6,7 +6,11 @@ from ska.cdm.messages.subarray_node.scan import ScanRequest
 from ska.cdm.schemas.subarray_node.scan import ScanRequestSchema
 from ska.cdm.utils import json_is_equal
 
-VALID_SCAN_REQUEST = '{"id": 1}'
+VALID_SCAN_REQUEST = """
+{   "interface": "https://schema.skatelescope.org/ska-low-tmc-scan/1.0",
+    "id": 1
+}
+"""
 
 
 def test_marshall_start_scan_request():
@@ -14,7 +18,8 @@ def test_marshall_start_scan_request():
     Verify that ScanRequest is marshalled to JSON correctly.
     """
     scan_id = 1
-    scan_request = ScanRequest(scan_id)
+    interface = "https://schema.skatelescope.org/ska-low-tmc-scan/1.0"
+    scan_request = ScanRequest(scan_id, interface)
     schema = ScanRequestSchema()
     result = schema.dumps(scan_request)
 
@@ -27,7 +32,6 @@ def test_unmarshall_start_scan_request():
     """
     schema = ScanRequestSchema()
     result = schema.loads(VALID_SCAN_REQUEST)
-    expected = ScanRequest(1)
-
+    expected = ScanRequest(1, "https://schema.skatelescope.org/ska-low-tmc-scan/1.0")
     assert result.scan_id is not None
     assert result == expected
