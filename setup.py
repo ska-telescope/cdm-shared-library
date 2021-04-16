cb36ffa592a538fdc12deae05e9eb8efb9a156d2
Andrew McDermott <andrew.mcdermott@stfc.ac.uk>
2021-04-16 13:49:49 +0100

    bumped to version 4.0.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open('README.md') as readme_file:
 
 setuptools.setup(
     name='cdm-shared-library',
-    version="3.0.0",
+    version="4.0.0",
     description="Configuration data model library",
     long_description=readme + '\n\n',
     author="Hélder Ribeiro",
