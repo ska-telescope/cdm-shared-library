041d7ab6209877925e8f56f2e11c0326326b33bd
Stewart Williams <3161869-bravostuzero@users.noreply.gitlab.com>
2021-05-04 13:21:58 +0100

    AT2-728 add schema validation to MCCSController.ReleaseResources

--- tests/unit/ska/cdm/schemas/mccscontroller/test_releaseresources.py
+++ tests/unit/ska/cdm/schemas/mccscontroller/test_releaseresources.py
@@ -1,17 +1,28 @@
 """
 Unit tests for ska.cdm.schemas.mccscontroller.releaseresources module.
 """
+import copy
 
+import pytest
+
+from ska.cdm.exceptions import JsonValidationError
 from ska.cdm.messages.mccscontroller.releaseresources import ReleaseResourcesRequest
-from ska.cdm.schemas.mccscontroller.releaseresources import (
-    ReleaseResourcesRequestSchema
-)
+from ska.cdm.schemas.mccscontroller.releaseresources import ReleaseResourcesRequestSchema
+from ska.cdm.schemas.shared import ValidatingSchema
 from ska.cdm.utils import json_is_equal
 
 VALID_JSON = """
 {
   "interface": "https://schema.skatelescope.org/ska-low-mccs-releaseresources/1.0",
   "subarray_id": 1,
+  "release_all": true
+}
+"""
+
+INVALID_JSON = """
+{
+  "interface": "https://schema.skatelescope.org/ska-low-mccs-releaseresources/1.0",
+  "subarray_id": -1,
   "release_all": true
 }
 """
@@ -38,3 +49,32 @@ def test_unmarshal_releaseresourcesrequest():
     """
     unmarshalled = ReleaseResourcesRequestSchema().loads(VALID_JSON)
     assert unmarshalled == VALID_OBJECT
+
+
+def test_deserialising_invalid_json_raises_exception_when_strict():
+    schema = ReleaseResourcesRequestSchema()
+    schema.context[ValidatingSchema.VALIDATE] = True
+    schema.context[ValidatingSchema.VALIDATION_STRICTNESS] = 2
+
+    with pytest.raises(JsonValidationError):
+        _ = schema.loads(INVALID_JSON)
+
+
+def test_serialising_invalid_object_raises_exception_when_strict():
+    o = copy.deepcopy(VALID_OBJECT)
+    o.subarray_id = -1
+
+    schema = ReleaseResourcesRequestSchema()
+    schema.context[ValidatingSchema.VALIDATE] = True
+    schema.context[ValidatingSchema.VALIDATION_STRICTNESS] = 2
+
+    with pytest.raises(JsonValidationError):
+        _ = schema.dumps(o)
+
+
+def test_serialising_valid_object_does_not_raise_exception_when_strict():
+    schema = ReleaseResourcesRequestSchema()
+    schema.context[ValidatingSchema.VALIDATE] = True
+    schema.context[ValidatingSchema.VALIDATION_STRICTNESS] = 2
+
+    _ = schema.dumps(VALID_OBJECT)
