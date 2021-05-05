9c1d0941aa9ea9d526c409cda4308cb6cd03362d
Stewart Williams <3161869-bravostuzero@users.noreply.gitlab.com>
2021-05-05 11:54:57 +0100

    AT2-728 add schema validation to SubArrayNode.scan

--- src/ska/cdm/schemas/subarray_node/scan.py
+++ src/ska/cdm/schemas/subarray_node/scan.py
@@ -3,22 +3,26 @@ The schemas module defines Marshmallow schemas that map CDM message classes
 and data model classes to/from a JSON representation.
 """
 
-from marshmallow import Schema, fields, post_load, post_dump
+from marshmallow import fields, post_load, post_dump
 
 import ska.cdm.messages.subarray_node.scan as scan_msgs
 from ska.cdm.schemas import CODEC
+from ska.cdm.schemas.shared import ValidatingSchema
 
 __all__ = ["ScanRequestSchema"]
 
 
 @CODEC.register_mapping(scan_msgs.ScanRequest)
-class ScanRequestSchema(Schema):  # pylint: disable=too-few-public-methods
+class ScanRequestSchema(ValidatingSchema):  # pylint: disable=too-few-public-methods
     """
     Create the Schema for ScanDuration using timedelta
     """
 
-    scan_id = fields.Integer(data_key="id")
     interface = fields.String()
+    # holds scan ID for MID
+    id = fields.Integer()
+    # holds scan ID for LOW
+    scan_id = fields.Integer()
 
     @post_load
     def create_scan_request(self, data, **_):  # pylint: disable=no-self-use
@@ -29,9 +33,19 @@ class ScanRequestSchema(Schema):  # pylint: disable=too-few-public-methods
         :param _: kwargs passed by Marshmallow
         :return: ScanRequest instance populated to match JSON
         """
-        scan_id = data["scan_id"]
-        interface = data.get("interface", None)
-        return scan_msgs.ScanRequest(scan_id, interface)
+
+        is_low = 'low' in data.get("interface", '')
+        if is_low:
+            scan_id = data["scan_id"]
+            interface = data["interface"]
+        else:
+            scan_id = data["id"]
+            interface = None
+
+        return scan_msgs.ScanRequest(
+            scan_id=scan_id,
+            interface=interface
+        )
 
     @post_dump
     def filter_nulls(self, data, **_):
@@ -42,4 +56,13 @@ class ScanRequestSchema(Schema):  # pylint: disable=too-few-public-methods
         :param _: kwargs passed by Marshmallow
         :return: dict suitable for SubArrayNode configuration
         """
-        return {k: v for k, v in data.items() if v is not None}
+        # filter out nulls
+        data = {k: v for k, v in data.items() if v is not None}
+
+        # set scan ID key name appropriately for telescope
+        is_low = 'low' in data.get('interface', '')
+        if not is_low:
+            data['id'] = data['scan_id']
+            del data['scan_id']
+
+        return data
--- tests/unit/ska/cdm/schemas/subarray_node/test_scan.py
+++ tests/unit/ska/cdm/schemas/subarray_node/test_scan.py
@@ -2,65 +2,112 @@
 Unit tests for the ska.cdm.schemas.subarray_node.scan module
 """
 
+import copy
+
+import pytest
+
+from ska.cdm.exceptions import JsonValidationError
 from ska.cdm.messages.subarray_node.scan import ScanRequest
+from ska.cdm.schemas.shared import ValidatingSchema
 from ska.cdm.schemas.subarray_node.scan import ScanRequestSchema
 from ska.cdm.utils import json_is_equal
 
-VALID_SCAN_REQUEST = """
+VALID_MID_SCANREQUEST_JSON = """
 {
     "id": 1
 }
 """
 
-VALID_LOW_SCAN_REQUEST = """
-{   "interface": "https://schema.skatelescope.org/ska-low-tmc-scan/1.0",
-    "id": 1
+VALID_MID_SCANREQUEST_OBJECT = ScanRequest(
+    scan_id=1
+)
+
+VALID_LOW_SCANREQUEST_JSON = """
+{   
+    "interface": "https://schema.skatelescope.org/ska-low-tmc-scan/1.0",
+    "scan_id": 1
+}
+"""
+
+VALID_LOW_SCANREQUEST_OBJECT = ScanRequest(
+    interface="https://schema.skatelescope.org/ska-low-tmc-scan/1.0",
+    scan_id=1
+)
+
+INVALID_LOW_SCANREQUEST_JSON = """
+{   
+    "interface": "https://schema.skatelescope.org/ska-low-tmc-scan/1.0",
+    "scan_id": -1.3
 }
 """
 
 
-def test_marshall_start_scan_request():
+@pytest.mark.parametrize('instance, expected', [
+    (VALID_MID_SCANREQUEST_OBJECT, VALID_MID_SCANREQUEST_JSON),
+    (VALID_LOW_SCANREQUEST_OBJECT, VALID_LOW_SCANREQUEST_JSON)
+])
+def test_marshal(instance, expected):
     """
-    Verify that ScanRequest is marshalled to JSON correctly.
-    """
-    scan_id = 1
-    scan_request = ScanRequest(scan_id)
-    schema = ScanRequestSchema()
-    result = schema.dumps(scan_request)
-
-    assert json_is_equal(result, VALID_SCAN_REQUEST)
-
-
-def test_unmarshall_start_scan_request():
-    """
-    Verify that JSON can be unmarshalled back to a ScanRequest
+    Verify that objects are marshaled to JSON correctly.
     """
     schema = ScanRequestSchema()
-    result = schema.loads(VALID_SCAN_REQUEST)
-    expected = ScanRequest(1)
-    assert result.scan_id is not None
-    assert result == expected
+    json_str = schema.dumps(instance)
+    assert json_is_equal(json_str, expected)
 
 
-def test_marshall_start_scan_request_for_low():
+@pytest.mark.parametrize('json_str, expected', [
+    (VALID_MID_SCANREQUEST_JSON, VALID_MID_SCANREQUEST_OBJECT),
+    (VALID_LOW_SCANREQUEST_JSON, VALID_LOW_SCANREQUEST_OBJECT)
+])
+def test_unmarshal(json_str, expected):
     """
-    Verify that ScanRequest is marshalled to JSON correctly.
-    """
-    scan_id = 1
-    interface = "https://schema.skatelescope.org/ska-low-tmc-scan/1.0"
-    scan_request = ScanRequest(scan_id, interface)
-    schema = ScanRequestSchema()
-    result = schema.dumps(scan_request)
-
-    assert json_is_equal(result, VALID_LOW_SCAN_REQUEST)
-
-
-def test_unmarshall_start_scan_request_for_low():
-    """
-    Verify that JSON can be unmarshalled back to a ScanRequest
+    Verify that JSON can be unmarshaled back to objects.
     """
     schema = ScanRequestSchema()
-    result = schema.loads(VALID_LOW_SCAN_REQUEST)
-    expected = ScanRequest(1, "https://schema.skatelescope.org/ska-low-tmc-scan/1.0")
-    assert result.scan_id is not None
-    assert result == expected
+    unmarshaled = schema.loads(json_str)
+    assert unmarshaled == expected
+
+
+def test_deserialising_invalid_json_raises_exception_when_strict():
+    """
+    Verify that an exception is raised when invalid JSON is deserialised in
+    strict mode.
+    """
+    schema = ScanRequestSchema()
+    schema.context[ValidatingSchema.VALIDATE] = True
+    schema.context[ValidatingSchema.VALIDATION_STRICTNESS] = 2
+
+    with pytest.raises(JsonValidationError):
+        _ = schema.loads(INVALID_LOW_SCANREQUEST_JSON)
+
+
+@pytest.mark.xfail(strict=True)
+def test_serialising_invalid_object_raises_exception_when_strict():
+    """
+    Verify that an exception is raised when an invalid object is serialised in
+    strict mode.
+
+    This test is xfailed as the telescope model schema for SubArrayNode.Scan
+    does not impose any constraints.
+    """
+    o = copy.deepcopy(VALID_LOW_SCANREQUEST_OBJECT)
+    o.scan_id = -1
+
+    schema = ScanRequestSchema()
+    schema.context[ValidatingSchema.VALIDATE] = True
+    schema.context[ValidatingSchema.VALIDATION_STRICTNESS] = 2
+
+    with pytest.raises(JsonValidationError):
+        _ = schema.dumps(o)
+
+
+def test_serialising_valid_object_does_not_raise_exception_when_strict():
+    """
+    Verify that an exception is not raised when a valid object is serialised
+    in strict mode.
+    """
+    schema = ScanRequestSchema()
+    schema.context[ValidatingSchema.VALIDATE] = True
+    schema.context[ValidatingSchema.VALIDATION_STRICTNESS] = 2
+
+    _ = schema.dumps(VALID_LOW_SCANREQUEST_OBJECT)
