0aa33d86345670a989ade01b1b8b54b99b31919c
Stewart Williams <3161869-bravostuzero@users.noreply.gitlab.com>
2021-05-13 15:03:58 +0100

    bumped to version 5.0.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open('README.md') as readme_file:
 
 setuptools.setup(
     name='cdm-shared-library',
-    version="5.0.1",
+    version="5.0.2",
     description="Configuration data model library",
     long_description=readme + '\n\n',
     author="Hélder Ribeiro",
