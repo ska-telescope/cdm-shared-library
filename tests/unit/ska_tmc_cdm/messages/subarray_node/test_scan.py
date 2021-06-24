8c42c62899d28ffe9ebe8d81f8c0369cb77db3f5
Andrew McDermott <andrew.mcdermott@stfc.ac.uk>
2021-06-24 15:43:24 +0100

    AT2-910: Modify SubArrayNode Scan

--- tests/unit/ska_tmc_cdm/messages/subarray_node/test_scan.py
+++ tests/unit/ska_tmc_cdm/messages/subarray_node/test_scan.py
@@ -2,16 +2,28 @@
 Unit tests for the SubarrayNode.Configure request/response mapper module.
 """
 
+import pytest
+
 from ska_tmc_cdm.messages.subarray_node.scan import ScanRequest
 
+MID_SDP_INTERFACE = "https://schema.skatelescope.org/ska-mid-sdp-scan/1.0"
+LOW_TMC_INTERFACE = "https://schema.skatelescope.org/ska-low-tmc-scan/1.0"
 
+
+@pytest.mark.xfail
 def test_scan_request_init():
     """
     Create a ScanRequest instance with a scan id of 2 and verify __eq__ behaviour.
     """
     scan_id = 2
-    scan_request = ScanRequest(scan_id)
-    scan_request_2 = ScanRequest(scan_id)
+    scan_request = ScanRequest(
+        interface = MID_SDP_INTERFACE,
+        scan_id = scan_id,
+    )
+    scan_request_2 = ScanRequest(
+        interface = MID_SDP_INTERFACE,
+        scan_id = scan_id,
+    )
 
     empty_object = {}
 
@@ -28,12 +40,12 @@ def test_scan_request_init_for_low():
     """
     scan_id = 2
     scan_request = ScanRequest(
-        interface='https://schema.skatelescope.org/ska-low-tmc-scan/1.0',
+        interface=LOW_TMC_INTERFACE,
         scan_id=scan_id,
     )
     scan_request_2 = ScanRequest(
         scan_id=scan_id,
-        interface='https://schema.skatelescope.org/ska-low-tmc-scan/1.0'
+        interface=LOW_TMC_INTERFACE
     )
 
     empty_object = {}
