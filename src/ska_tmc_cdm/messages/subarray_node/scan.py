5a29bac98d5b28e7dc66567704b1ae491aa4c607
Andrew McDermott <andrew.mcdermott@stfc.ac.uk>
2021-06-28 09:49:55 +0100

    AT2-855: further changes to JSON

--- src/ska_tmc_cdm/messages/subarray_node/scan.py
+++ src/ska_tmc_cdm/messages/subarray_node/scan.py
@@ -11,7 +11,13 @@ class ScanRequest:  # pylint: disable=too-few-public-methods
     ScanRequest represents the request argument for SubArrayNode.scan call.
     """
 
-    def __init__(self, scan_id: int, interface: str = None):
+    def __init__(
+            self, 
+            scan_id: int, 
+            interface: str = None, 
+            transaction_id: str = None
+        ):
+        self.transaction_id = transaction_id
         self.interface = interface
         self.scan_id = scan_id
 
@@ -19,4 +25,5 @@ class ScanRequest:  # pylint: disable=too-few-public-methods
         if not isinstance(other, ScanRequest):
             return False
         return self.interface == other.interface and \
-               self.scan_id == other.scan_id
+               self.scan_id == other.scan_id and \
+               self.transaction_id == other.transaction_id
