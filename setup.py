949803a5532d6fd61d92cc5fc396f4090dd0f33b
Stewart Williams <3161869-bravostuzero@users.noreply.gitlab.com>
2021-07-29 09:02:07 +0100

    bumped to version 6.0.3

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open('README.md') as readme_file:
 
 setuptools.setup(
     name='ska-tmc-cdm',
-    version="6.0.2",
+    version="6.0.3",
     description="SKA Control Data Model",
     long_description=readme + '\n\n',
     author="Hélder Ribeiro",
