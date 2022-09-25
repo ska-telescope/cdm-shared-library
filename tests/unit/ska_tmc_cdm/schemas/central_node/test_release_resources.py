fa98a36cde83a9d705ee6bc4ca89df9118f5506e
Manish Lalwani <manish_lalwani11@persistent.com>
2022-09-26 02:08:43 +0530

    Added support for Expand design pattern for Mid telescope

--- tests/unit/ska_tmc_cdm/schemas/central_node/test_release_resources.py
+++ tests/unit/ska_tmc_cdm/schemas/central_node/test_release_resources.py
@@ -6,18 +6,17 @@ import pytest
 
 from ska_tmc_cdm.messages.central_node.common import DishAllocation
 from ska_tmc_cdm.messages.central_node.release_resources import ReleaseResourcesRequest
-from ska_tmc_cdm.schemas.central_node.release_resources import (
-    ReleaseResourcesRequestSchema,
-)
-
-from .. import utils
+from ska_tmc_cdm.schemas import CODEC
+from ska_tmc_cdm.utils import assert_json_is_equal
 
 VALID_MID_PARTIAL_RELEASE_JSON = """
 {
     "interface": "https://schema.skao.int/ska-tmc-releaseresources/2.0",
     "transaction_id": "txn-blah-blah-00001",
     "subarray_id": 1, 
-    "receptor_ids": ["0001", "0002"]
+    "receptor_ids": ["0001", "0002"],
+    "sdp_id": "sbi-mvp01-20220919-00001",
+    "sdp_max_length" : 125.40
 }
 """
 
@@ -26,13 +25,17 @@ VALID_MID_PARTIAL_RELEASE_OBJECT = ReleaseResourcesRequest(
     transaction_id="txn-blah-blah-00001",
     subarray_id=1,
     dish_allocation=DishAllocation(receptor_ids=["0001", "0002"]),
+    sdp_id="sbi-mvp01-20220919-00001",
+    sdp_max_length=125.40,
 )
 
 VALID_MID_FULL_RELEASE_JSON = """
 {
     "interface": "https://schema.skao.int/ska-tmc-releaseresources/2.0",
     "subarray_id": 1,
-    "release_all": true
+    "release_all": true,
+    "sdp_id": "sbi-mvp01-20220919-00001",
+    "sdp_max_length" : 125.40
 }
 """
 
@@ -40,6 +43,8 @@ VALID_MID_FULL_RELEASE_OBJECT = ReleaseResourcesRequest(
     interface="https://schema.skao.int/ska-tmc-releaseresources/2.0",
     subarray_id=1,
     release_all=True,
+    sdp_id="sbi-mvp01-20220919-00001",
+    sdp_max_length=125.40,
 )
 
 # mixed partial / full request, used to test which params are ignored
@@ -48,13 +53,17 @@ VALID_MID_MIXED_ARGS_OBJECT = ReleaseResourcesRequest(
     subarray_id=1,
     release_all=True,
     dish_allocation=DishAllocation(receptor_ids=["0001", "0002"]),
+    sdp_id="sbi-mvp01-20220919-00001",
+    sdp_max_length=125.40,
 )
 
 VALID_LOW_FULL_RELEASE_JSON = """
 {
     "interface": "https://schema.skao.int/ska-low-tmc-releaseresources/2.0",
     "subarray_id": 1,
-    "release_all": true
+    "release_all": true,
+    "sdp_id": "sbi-mvp01-20220919-00001",
+    "sdp_max_length" : 125.40
 }
 """
 
@@ -62,13 +71,17 @@ VALID_LOW_FULL_RELEASE_OBJECT = ReleaseResourcesRequest(
     interface="https://schema.skao.int/ska-low-tmc-releaseresources/2.0",
     subarray_id=1,
     release_all=True,
+    sdp_id="sbi-mvp01-20220919-00001",
+    sdp_max_length=125.40,
 )
 
 INVALID_LOW_FULL_RELEASE_JSON = """
 {
     "interface": "https://schema.skao.int/ska-low-tmc-releaseresources/2.0",
     "subarray_id": -1,
-    "release_all": true
+    "release_all": true,
+    "sdp_id": "sbi-mvp01-20220919-00001",
+    "sdp_max_length" : 125.40
 }
 """
 
@@ -79,44 +92,27 @@ def low_invalidator_fn(o: ReleaseResourcesRequest):
 
 
 @pytest.mark.parametrize(
-    "schema_cls,instance,modifier_fn,valid_json,invalid_json",
+    "instance,valid_json",
     [
         (
-            ReleaseResourcesRequestSchema,
             VALID_MID_FULL_RELEASE_OBJECT,
-            None,  # no validation for MID
             VALID_MID_FULL_RELEASE_JSON,
-            None,
         ),  # no validation for MID
         (
-            ReleaseResourcesRequestSchema,
             VALID_MID_PARTIAL_RELEASE_OBJECT,
-            None,  # no validation for MID
             VALID_MID_PARTIAL_RELEASE_JSON,
-            None,
         ),  # no validation for MID
         (
-            ReleaseResourcesRequestSchema,
             VALID_MID_MIXED_ARGS_OBJECT,
-            None,  # no validation for MID
             VALID_MID_FULL_RELEASE_JSON,  # expect partial spec to be ignored
-            None,
         ),  # no validation for MID
-        (
-            ReleaseResourcesRequestSchema,
-            VALID_LOW_FULL_RELEASE_OBJECT,
-            low_invalidator_fn,
-            VALID_LOW_FULL_RELEASE_JSON,
-            INVALID_LOW_FULL_RELEASE_JSON,
-        ),
+        (VALID_LOW_FULL_RELEASE_OBJECT, VALID_LOW_FULL_RELEASE_JSON),
     ],
 )
-def test_releaseresources_serialisation_and_validation(
-    schema_cls, instance, modifier_fn, valid_json, invalid_json
-):
+def test_releaseresources_serialisation_and_validation(instance, valid_json):
     """
     Verifies that the schema marshals, unmarshals, and validates correctly.
     """
-    utils.test_schema_serialisation_and_validation(
-        schema_cls, instance, modifier_fn, valid_json, invalid_json
-    )
+
+    marshalled = CODEC.dumps(instance)
+    assert_json_is_equal(marshalled, valid_json)
