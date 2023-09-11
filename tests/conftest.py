d7e51377bce9392d573c7261b81a428ea1ff7df6
Brendan McCollam <brendan.mccollam@stfc.ac.uk>
2023-09-11 18:16:32 +0530

    BTN-2072: fmt

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,8 @@ import pytest
 
 HERE = Path(__file__).parent.resolve()
 
+
 @pytest.fixture(autouse=True)
 def local_tmdata(monkeypatch):
-	fixture_tmdata = str(HERE / "unit/fixture_data/tmdata")
-	monkeypatch.setenv("SKA_TELMODEL_SOURCES", fixture_tmdata)
+    fixture_tmdata = str(HERE / "unit/fixture_data/tmdata")
+    monkeypatch.setenv("SKA_TELMODEL_SOURCES", fixture_tmdata)
