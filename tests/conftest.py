c718321a99948b2c7593dff8d5876068fa776778
Brendan McCollam <brendan.mccollam@stfc.ac.uk>
2023-09-19 12:49:55 +0100

    BTN-2072: Git submodule with sparse checkout

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,5 +7,5 @@ HERE = Path(__file__).parent.resolve()
 
 @pytest.fixture(autouse=True)
 def local_tmdata(monkeypatch):
-    fixture_tmdata = str(HERE / "unit/fixture_data/tmdata")
+    fixture_tmdata = str(HERE / "unit/fixture_data/ska-telmodel/tmdata")
     monkeypatch.setenv("SKA_TELMODEL_SOURCES", fixture_tmdata)
