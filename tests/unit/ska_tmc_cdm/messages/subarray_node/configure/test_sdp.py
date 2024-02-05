6a9c27f3ec0215ef8b6c6ed15e3282468ec52af4
Manish Lalwani <manish_lalwani11@persistent.com>
2024-02-06 03:23:08 +0530

    NAK-787 Modified core,mccs,sdp,tmc test cases

--- tests/unit/ska_tmc_cdm/messages/subarray_node/configure/test_sdp.py
+++ tests/unit/ska_tmc_cdm/messages/subarray_node/configure/test_sdp.py
@@ -1,26 +1,32 @@
 """
 Unit tests for the ska_tmc_cdm.messages.subarray_node.configure.sdp module.
 """
-from ska_tmc_cdm.messages.subarray_node.configure.sdp import SDPConfiguration
+import pytest
+
+from tests.unit.ska_tmc_cdm.builder.subarray_node.sdp import SDPConfigurationBuilder
 
 
-def test_sdp_configuration_equals():
+@pytest.mark.parametrize(
+    "sdp_config_a,sdp_config_b,is_equal",
+    [
+        (
+            SDPConfigurationBuilder().set_scan_type("calibration_A").build(),
+            SDPConfigurationBuilder().set_scan_type("calibration_A").build(),
+            True,
+        ),
+        (
+            SDPConfigurationBuilder().set_scan_type("calibration_A").build(),
+            SDPConfigurationBuilder().set_scan_type("calibration_B").build(),
+            False,
+        ),
+    ],
+)
+def test_sdp_configuration_equality(sdp_config_a, sdp_config_b, is_equal):
     """
-    Verify that SDP configuration objects are equal when they have the same value
-    """
-    sdp_config_a = SDPConfiguration(scan_type="calibration_A")
-    sdp_config_b = SDPConfiguration(scan_type="calibration_A")
-
-    assert sdp_config_a == sdp_config_b
-
-    assert sdp_config_a != SDPConfiguration(scan_type="science_B")
-    assert sdp_config_b != SDPConfiguration(scan_type="science_B")
-
-
-def test_sdp_config_not_equal_to_other_objects():
-    """
-    Verify that SDP configuration objects are not considered equal to objects of
+    Verify that SDP configuration objects are equal when they have the same value, not equal for different value
+    And that SDP configuration objects are not considered equal to objects of
     other types.
     """
-    sdp_config = SDPConfiguration(scan_type="calibration_A")
-    assert sdp_config != 1
+    assert (sdp_config_a == sdp_config_b) == is_equal
+    assert sdp_config_a != 1
+    assert sdp_config_b != object
