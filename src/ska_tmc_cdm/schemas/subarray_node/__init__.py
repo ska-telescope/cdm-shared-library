6ca594b04dd50e3053af0c4158728244166d813d
Brendan McCollam <brendan.mccollam@stfc.ac.uk>
2024-06-06 16:57:44 +0530

    BTN-2367: Delete a lot of stuff in schemas/

--- src/ska_tmc_cdm/schemas/subarray_node/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-The schemas.subarray_node package contains Marshmallow schemas that convert
-JSON to/from the Python classes contained in ska_tmc_cdm.messages.subarray_node.
-"""
-# imports not used but required
-from . import assigned_resources, configure, scan  # noqa F401
