279c396923d18c8e8bd6be6845fefc1995f18c15
Brendan McCollam <brendan.mccollam@stfc.ac.uk>
2024-06-27 15:02:34 +0100

    BTN-2384: Adds SpecialTarget() for non-sidereal targets
    BTN-2384: Basic test
    
    BTN-2384: Fix tests
    
    BTN-2384: Test PointingConfiguration deserialisation
    
    BTN-2384: Docstring typo

--- tests/unit/ska_tmc_cdm/serialisation/subarray_node/configure/test_core.py
+++ tests/unit/ska_tmc_cdm/serialisation/subarray_node/configure/test_core.py
@@ -1,6 +1,7 @@
 """
 Unit tests for the ska_tmc_cdm.subarray_node.configure.common module.
 """
+import json
 from typing import NamedTuple
 
 import pytest
@@ -8,40 +9,44 @@ import pytest
 from ska_tmc_cdm import CODEC
 from ska_tmc_cdm.messages.subarray_node.configure.core import (
     DishConfiguration,
+    PointingConfiguration,
     ReceiverBand,
+    SolarSystemObject,
+    SpecialTarget,
     Target,
+    TargetUnion,
 )
 from ska_tmc_cdm.utils import assert_json_is_equal
 
 
 class Case(NamedTuple):
-    target: Target
+    target: TargetUnion
     json: str
 
 
-VALID_TARGET_JSON = """
-{
-  "ra": "12:34:56.78",
-  "dec": "+12:34:56.78",
-  "reference_frame": "ICRS",
-  "target_name": "NGC123"
+VALID_TARGET_JSON = {
+    "ra": "12:34:56.78",
+    "dec": "+12:34:56.78",
+    "reference_frame": "ICRS",
+    "target_name": "NGC123",
 }
-"""
 
-OFFSET_TARGET_JSON = """
-{
-  "ra": "12:34:56.78",
-  "dec": "+12:34:56.78",
-  "reference_frame": "ICRS",
-  "target_name": "NGC123",
-  "ca_offset_arcsec": 25.0,
-  "ie_offset_arcsec": -25.0
+
+OFFSET_TARGET_JSON = {
+    "ra": "12:34:56.78",
+    "dec": "+12:34:56.78",
+    "reference_frame": "ICRS",
+    "target_name": "NGC123",
+    "ca_offset_arcsec": 25.0,
+    "ie_offset_arcsec": -25.0,
 }
-"""
+
+
+NON_SIDEREAL_TARGET_JSON = {"name": "Sun", "reference_frame": "SPECIAL"}
 
 VALID_DISH_CONFIGURATION_JSON = '{"receiver_band": "5a"}'
 
-TEST_CASES = (
+TARGET_PAIRS = (
     Case(
         Target(ra="12h34m56.78s", dec="+12d34m56.78s", target_name="NGC123"),
         VALID_TARGET_JSON,
@@ -66,27 +71,41 @@ TEST_CASES = (
         ),
         OFFSET_TARGET_JSON,
     ),
+    Case(
+        SpecialTarget(name=SolarSystemObject.SUN, reference_frame="special"),
+        NON_SIDEREAL_TARGET_JSON,
+    ),
 )
 
 
-@pytest.mark.parametrize("target,expected", TEST_CASES)
+@pytest.mark.parametrize("target,expected", TARGET_PAIRS)
 def test_marshall_target_to_json(target, expected):
     """
     Verify that PointingConfiguration Target is marshalled to JSON correctly.
     """
     json_str = CODEC.dumps(target)
-    assert_json_is_equal(json_str, expected)
+    assert_json_is_equal(json_str, json.dumps(expected))
 
 
-def test_unmarshall_target_from_json():
+@pytest.mark.parametrize("target,jsonable_dict", TARGET_PAIRS)
+def test_unmarshall_target_from_json(target, jsonable_dict):
     """
     Verify that a Target is unmarshalled correctly from JSON.
     """
-    expected = Target(
-        ra="12h34m56.78s", dec="+12d34m56.78s", target_name="NGC123"
-    )
-    unmarshalled = CODEC.loads(Target, VALID_TARGET_JSON)
-    assert unmarshalled == expected
+    unmarshalled = CODEC.loads(target.__class__, json.dumps(jsonable_dict))
+    assert unmarshalled == target
+
+
+@pytest.mark.parametrize("target,jsonable_dict", TARGET_PAIRS)
+def test_unmarshall_pointing_parameters(target, jsonable_dict):
+    """
+    Verify that PointingParameters unmarshall correctly from JSON
+    and the right Target class is resolved.
+    """
+    pointing_configuration = PointingConfiguration(target=target)
+    json_str = json.dumps({"target": jsonable_dict})
+    unmarshalled = CODEC.loads(PointingConfiguration, json_str)
+    assert unmarshalled == pointing_configuration
 
 
 def test_marshall_dish_configuration_to_json():
