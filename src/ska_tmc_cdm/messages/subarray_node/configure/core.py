43740c03385169fd0250a7a1241c6018b71bf89b
Brendan McCollam <brendan.mccollam@stfc.ac.uk>
2024-07-05 12:17:10 +0100

    BTN-2421: is not None reads nicer

--- src/ska_tmc_cdm/messages/subarray_node/configure/core.py
+++ src/ska_tmc_cdm/messages/subarray_node/configure/core.py
@@ -160,7 +160,7 @@ class Target(TargetBase, CdmObject):
             del data["reference_frame"]
         else:
             # For the type checker. We know coord is not-none here.
-            assert bool(self.coord)
+            assert self.coord is not None
             # TODO: IMHO doing this conversion here is janky. If we only want to
             # work with ICRS coordinates, we should enforce that as part of
             # validation, not convert to it at the end when we dump()
@@ -185,7 +185,7 @@ class Target(TargetBase, CdmObject):
     @model_validator(mode="after")
     def ra_dec_or_offsets_required(self) -> Self:
         offsets = self.ca_offset_arcsec or self.ie_offset_arcsec
-        if not bool(self.coord) and not offsets:
+        if self.coord is None and not offsets:
             raise ValueError(
                 "A Target() must specify either ra/dec or one nonzero ca_offset_arcsec or ie_offset_arcsec"
             )
@@ -234,8 +234,8 @@ class Target(TargetBase, CdmObject):
             assert self_coord.dec is not None
             raw_ra = self_coord.ra.value
             raw_dec = self_coord.dec.value
-            assert bool(self_coord.ra.unit)
-            assert bool(self_coord.dec.unit)
+            assert self_coord.ra.unit is not None
+            assert self_coord.dec.unit is not None
             units = (self_coord.ra.unit.name, self_coord.dec.unit.name)
             reference_frame = self_coord.frame.name
             target_name = self.target_name
