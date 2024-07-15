a9073aabcc9f37118187b5244e1f144e22005cdb
Dayanand Arade <dayanand_arade@persistent.com>
2024-07-15 17:46:18 +0530

    NAK-949 NAK-977 added low configure test case

--- tests/fixtures/builder_pattern/configure_resource.py
+++ tests/fixtures/builder_pattern/configure_resource.py
@@ -40,6 +40,14 @@ from tests.unit.ska_tmc_cdm.builder.subarray_node.configure.sdp import (
     SDPConfigurationBuilder,
 )
 
+from tests.unit.ska_tmc_cdm.builder.subarray_node.configure.pst import (
+    PSTBeamConfigurationBuilder,
+    PSTChannelizationStageConfigurationBuilder,
+    PSTConfigurationBuilder,
+    PSTScanConfigurationBuilder,
+    PSTScanCoordinatesBuilder,
+)
+
 
 @pytest.fixture(scope="module")
 def station_beam_config() -> SubarrayBeamConfiguration:
@@ -155,6 +163,71 @@ def low_csp_config() -> CSPConfiguration:
             .set_frequency_band(ReceiverBand.BAND_1)
             .set_subarray_id(1)
             .set_band_5_tuning([5.85, 7.25])
+            .build()
+        )
+        .set_pst_config(
+            PSTConfigurationBuilder()
+            .set_beams(
+                [
+                    PSTBeamConfigurationBuilder()
+                    .set_beam_id(1)
+                    .set_scan(
+                        PSTScanConfigurationBuilder()
+                        .set_activation_time("2022-01-19T23:07:45Z")
+                        .set_bits_per_sample(32)
+                        .set_num_of_polarizations(2)
+                        .set_udp_nsamp(32)
+                        .set_wt_nsamp(32)
+                        .set_udp_nchan(24)
+                        .set_num_frequency_channels(432)
+                        .set_centre_frequency(200000000.0)
+                        .set_total_bandwidth(1562500.0)
+                        .set_observation_mode("VOLTAGE_RECORDER")
+                        .set_observer_id("jdoe")
+                        .set_project_id("project1")
+                        .set_pointing_id("pointing1")
+                        .set_source("J1921+2153")
+                        .set_itrf([5109360.133, 2006852.586, -3238948.127])
+                        .set_receiver_id("receiver3")
+                        .set_feed_polarization("LIN")
+                        .set_feed_handedness(1)
+                        .set_feed_angle(1.234)
+                        .set_feed_tracking_mode("FA")
+                        .set_feed_position_angle(10.0)
+                        .set_oversampling_ratio([8, 7])
+                        .set_coordinates(
+                            PSTScanCoordinatesBuilder()
+                            .set_equinox(2000.0)
+                            .set_ra("19:21:44.815")
+                            .set_dec("21:53:02.400")
+                            .build()
+                        )
+                        .set_max_scan_length(20000.0)
+                        .set_subint_duration(30.0)
+                        .set_receptors(["receptor1", "receptor2"])
+                        .set_receptor_weights([0.4, 0.6])
+                        .set_num_channelization_stages(2)
+                        .set_channelization_stages(
+                            [
+                                PSTChannelizationStageConfigurationBuilder()
+                                .set_num_filter_taps(1)
+                                .set_filter_coefficients([1.0])
+                                .set_num_frequency_channels(1024)
+                                .set_oversampling_ratio([32, 27])
+                                .build(),
+                                PSTChannelizationStageConfigurationBuilder()
+                                .set_num_filter_taps(1)
+                                .set_filter_coefficients([1.0])
+                                .set_num_frequency_channels(256)
+                                .set_oversampling_ratio([4, 3])
+                                .build(),
+                            ]
+                        )
+                        .build()
+                    )
+                    .build()
+                ]
+            )
             .build()
         )
         .set_lowcbf(
