d74300fb025d525b83706dc9346ea98068f8e041
liz.bartlett <liz.bartlett@stfc.ac.uk>
2024-07-25 09:36:33 +0100

    STAR-558 adding valid mid csp configure 4.0 json

--- tests/unit/ska_tmc_cdm/serialisation/subarray_node/configure/test_csp.py
+++ tests/unit/ska_tmc_cdm/serialisation/subarray_node/configure/test_csp.py
@@ -220,6 +220,40 @@ VALID_LOW_CSP_JSON_PI20 = """{
     }
   }"""
 
+VALID_MID_CSP_CONFIGURE_JSON_4_0 = {
+    "csp": {
+        "interface": "https://schema.skao.int/ska-csp-configurescan/4.0",
+        "common": {
+            "config_id": "sbi-mvp01-20200325-00001-science_A",
+            "frequency_band": "1"
+        },
+        "midcbf": {
+            "frequency_band_offset_stream1": 80,
+            "correlation": {
+                "processing_regions": [{
+                    "fsp_ids": [1, 2, 3, 4],
+                    "receptors": ["SKA063", "SKA001", "SKA100"],
+                    "start_freq": 350000000,
+                    "channel_width": 13440,
+                    "channel_count": 52080,
+                    "sdp_start_channel_id": 0,
+                    "integration_factor": 1
+                }, {
+                    "fsp_ids": [1],
+                    "start_freq": 548437600,
+                    "channel_width": 13440,
+                    "channel_count": 14880,
+                    "sdp_start_channel_id": 1,
+                    "integration_factor": 10
+                }]
+            },
+            "vlbi": {}
+        },
+        "pss": {},
+        "pst": {}
+    }
+}
+
 
 def test_marshall_fsp_configuration_with_undefined_optional_parameters():
     """
