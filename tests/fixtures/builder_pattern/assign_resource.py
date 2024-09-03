7161ed297559767b364a411e739dff107f1ae323
Brendan McCollam <brendan.mccollam@stfc.ac.uk>
2024-09-03 14:15:29 +0100

    BTN-2456: Better builders for SDP

--- tests/fixtures/builder_pattern/assign_resource.py
+++ tests/fixtures/builder_pattern/assign_resource.py
@@ -19,63 +19,6 @@ from tests.unit.ska_tmc_cdm.builder.central_node.sdp import (
 
 
 @pytest.fixture(scope="module")
-def processing_block_parameters():
-    """
-    Provides Processing block parameters
-    """
-    return {
-        "plasmaEnabled": True,
-        "reception": {
-            "layout": "http://127.0.0.1:80/model/default/ska1_low/layout",
-            "num_channels": 13824,
-            "channels_per_stream": 6912,
-            "continuous_mode": True,
-            "transport_protocol": "tcp",
-        },
-        "pvc": {"name": "receive-data"},
-        "plasma_parameters": {
-            "initContainers": [
-                {
-                    "name": "existing-output-remover",
-                    "image": "artefact.skao.int/ska-sdp-realtime-receive-modules:3.3.0",
-                    "command": ["rm", "-rf", "/mnt/data/output*.ms"],
-                    "volumeMounts": [
-                        {"mountPath": "/mnt/data", "name": "receive-data"}
-                    ],
-                }
-            ],
-            "extraContainers": [
-                {
-                    "name": "plasma-processor",
-                    "image": "artefact.skao.int/ska-sdp-realtime-receive-modules:3.3.0",
-                    "command": [
-                        "plasma-mswriter",
-                        "-s",
-                        "/plasma/socket",
-                        "--max_payloads",
-                        "12",
-                        "--use_plasmastman",
-                        "False",
-                        "/mnt/data/output.ms",
-                    ],
-                    "volumeMounts": [
-                        {
-                            "name": "plasma-storage-volume",
-                            "mountPath": "/plasma",
-                        },
-                        {"mountPath": "/mnt/data", "name": "receive-data"},
-                    ],
-                },
-                {
-                    "name": "tmlite-server",
-                    "image": "artefact.skao.int/ska-sdp-tmlite-server:0.3.0",
-                },
-            ],
-        },
-    }
-
-
-@pytest.fixture(scope="module")
 def channel() -> Channel:
     return ChannelBuilder()
 
@@ -96,112 +39,46 @@ def sdp_workflow():
 
 
 @pytest.fixture(scope="module")
-def processing_block(processing_block_parameters):
-
-    return ProcessingBlockConfigurationBuilder(
-        parameters=processing_block_parameters
-    )
+def processing_block():
+    return ProcessingBlockConfigurationBuilder()
 
 
 @pytest.fixture(scope="module")
 def beams():
-    """
-    Provides CDM Beam configuration instance through BeamConfigurationBuilder builder class
-    """
-    return (
-        BeamConfigurationBuilder()
-        .set_beam_id(beam_id="vis0")
-        .set_function(function="visibilities")
-        .build()
-    )
+    return BeamConfigurationBuilder()
 
 
 @pytest.fixture(scope="module")
-def channels(channel):
-    """
-    Provides CDM Channel configuration instance through ChannelConfigurationBuilder builder class
-    """
-    return (
-        ChannelConfigurationBuilder()
-        .set_channels_id(channels_id="vis_channels")
-        .set_spectral_windows(spectral_windows=[channel])
-        .build()
-    )
+def channels():
+    return ChannelConfigurationBuilder()
 
 
 @pytest.fixture(scope="module")
 def polarisation_config():
-    """
-    Provides CDM Polarisation configuration instance through PolarisationConfigurationBuilder builder class
-    """
-    return (
-        PolarisationConfigurationBuilder()
-        .set_polarisations_id(polarisations_id="all")
-        .set_corr_type(corr_type=["XX", "XY", "YY", "YX"])
-        .build()
-    )
+    return PolarisationConfigurationBuilder()
 
 
 @pytest.fixture(scope="module")
 def phase_dir():
-    """
-    Provides CDM PhaseDir configuration instance through PhaseDirBuilder builder class
-    """
-    return (
-        PhaseDirBuilder()
-        .set_ra(ra=[123, 0.1])
-        .set_dec(dec=[123, 0.1])
-        .set_reference_time(reference_time="...")
-        .set_reference_frame(reference_frame="ICRF3")
-        .build()
-    )
+    return PhaseDirBuilder()
 
 
 @pytest.fixture(scope="module")
-def field_config(phase_dir):
-    """
-    Provides CDM Field configuration instance through FieldConfigurationBuilder builder class
-    """
-    return (
-        FieldConfigurationBuilder()
-        .set_field_id(field_id="field_a")
-        .set_pointing_fqdn(pointing_fqdn="low-tmc/telstate/0/pointing")
-        .set_phase_dir(phase_dir=phase_dir)
-        .build()
-    )
+def field_config():
+    return FieldConfigurationBuilder()
 
 
 @pytest.fixture(scope="module")
-def execution_block(
-    beams, channels, polarisation_config, field_config, eb_scan_type
-):
+def execution_block():
     """
     Provides CDM Execution Block configuration instance through ExecutionBlockConfigurationBuilder builder class
     """
-    return (
-        ExecutionBlockConfigurationBuilder()
-        .set_eb_id(eb_id="eb-mvp01-20200325-00001")
-        .set_max_length(max_length=3600)
-        .set_beams(beams=[beams])
-        .set_channels(channels=[channels])
-        .set_polarisations(polarisations=[polarisation_config])
-        .set_fields(fields=[field_config])
-        .set_scan_types(scan_types=[eb_scan_type])
-        .build()
-    )
+    return ExecutionBlockConfigurationBuilder()
 
 
 @pytest.fixture(scope="module")
-def sdp_allocate(processing_block, execution_block, scan_type):
+def sdp_allocate():
     """
     Provides CDM SDP configuration instance through SDPConfigurationBuilder builder class
     """
-    return (
-        SDPConfigurationBuilder()
-        .set_eb_id(eb_id="sbi-mvp01-20200325-00001")
-        .set_max_length(max_length=100.0)
-        .set_scan_types(scan_types=[scan_type])
-        .set_processing_blocks(processing_blocks=[processing_block])
-        .set_execution_block(execution_block=execution_block)
-        .build()
-    )
+    return SDPConfigurationBuilder()
--- tests/unit/ska_tmc_cdm/builder/central_node/sdp.py
+++ tests/unit/ska_tmc_cdm/builder/central_node/sdp.py
@@ -64,379 +64,93 @@ ProcessingBlockConfigurationBuilder = functools.partial(
     ProcessingBlockConfiguration,
     pb_id="pb-mvp01-20200325-00001",
     workflow=SDPWorkflowBuilder(),
+    parameters={
+        "plasmaEnabled": True,
+        "reception": {
+            "layout": "http://127.0.0.1:80/model/default/ska1_low/layout",
+            "num_channels": 13824,
+            "channels_per_stream": 6912,
+            "continuous_mode": True,
+            "transport_protocol": "tcp",
+        },
+        "pvc": {"name": "receive-data"},
+        "plasma_parameters": {
+            "initContainers": [
+                {
+                    "name": "existing-output-remover",
+                    "image": "artefact.skao.int/ska-sdp-realtime-receive-modules:3.3.0",
+                    "command": ["rm", "-rf", "/mnt/data/output*.ms"],
+                    "volumeMounts": [
+                        {"mountPath": "/mnt/data", "name": "receive-data"}
+                    ],
+                }
+            ],
+            "extraContainers": [
+                {
+                    "name": "plasma-processor",
+                    "image": "artefact.skao.int/ska-sdp-realtime-receive-modules:3.3.0",
+                    "command": [
+                        "plasma-mswriter",
+                        "-s",
+                        "/plasma/socket",
+                        "--max_payloads",
+                        "12",
+                        "--use_plasmastman",
+                        "False",
+                        "/mnt/data/output.ms",
+                    ],
+                    "volumeMounts": [
+                        {
+                            "name": "plasma-storage-volume",
+                            "mountPath": "/plasma",
+                        },
+                        {"mountPath": "/mnt/data", "name": "receive-data"},
+                    ],
+                },
+                {
+                    "name": "tmlite-server",
+                    "image": "artefact.skao.int/ska-sdp-tmlite-server:0.3.0",
+                },
+            ],
+        },
+    },
+)
+
+BeamConfigurationBuilder = functools.partial(
+    BeamConfiguration,
+    beam_id="vis0",
+    function="visibilities",
 )
 
 
-class SDPConfigurationBuilder:
-    """
-    SDPConfigurationBuilder is a test data builder for CDM SDPConfiguration objects.
-
-    By default, SDPConfigurationBuilder will build an SDPConfiguration
-
-    for low observation command.
-    """
-
-    def __init__(self) -> "SDPConfigurationBuilder":
-        self.eb_id = None
-        self.max_length = None
-        self.scan_types = None
-        self.processing_blocks = None
-        self.execution_block = None
-        self.resources = None
-        self.interface = None
-
-    def set_eb_id(self, eb_id: str) -> "SDPConfigurationBuilder":
-        """
-        Set the eb id
-        :param eb_id: EB id
-        """
-        self.eb_id = eb_id
-        return self
-
-    def set_max_length(self, max_length: float) -> "SDPConfigurationBuilder":
-        """
-        Set the max length
-        :param max_length: Max length
-        """
-        self.max_length = max_length
-        return self
-
-    def set_scan_types(self, scan_types: list) -> "SDPConfigurationBuilder":
-        """
-        Set the scan types
-        :param scan_types: Scan types
-        """
-        self.scan_types = scan_types
-        return self
-
-    def set_processing_blocks(
-        self, processing_blocks: List[ProcessingBlockConfiguration]
-    ) -> "SDPConfigurationBuilder":
-        """
-        Set the processing blocks
-        :param processing_blocks: Processing blocks
-        """
-        self.processing_blocks = processing_blocks
-        return self
-
-    def set_execution_block(
-        self, execution_block: ExecutionBlockConfiguration
-    ) -> "SDPConfigurationBuilder":
-        """
-        Set the execution block
-        :param execution_block: Execution block
-        """
-        self.execution_block = execution_block
-        return self
-
-    def set_resources(self, resources: dict) -> "SDPConfigurationBuilder":
-        """
-        Set the resources
-        :param resources: Resources
-        """
-        self.resources = resources
-        return self
-
-    def set_interface(self, interface: str) -> "SDPConfigurationBuilder":
-        """
-        Set the interface
-        :param interface: Interface
-        """
-        self.interface = interface
-        return self
-
-    def build(self) -> SDPConfiguration:
-        """
-        Build or create CDM SDPConfiguration object
-        :return: CDM SDPConfiguration object
-        """
-        return SDPConfiguration(
-            eb_id=self.eb_id,
-            max_length=self.max_length,
-            scan_types=self.scan_types,
-            processing_blocks=self.processing_blocks,
-            execution_block=self.execution_block,
-            resources=self.resources,
-            interface=self.interface,
-        )
+ChannelConfigurationBuilder = functools.partial(
+    ChannelConfiguration,
+    channels_id="vis_channels",
+    spectral_windows=(ChannelBuilder(),),
+)
 
 
-class BeamConfigurationBuilder:
-    """
-    BeamConfigurationBuilder is a test data builder for CDM BeamConfiguration objects.
+PolarisationConfigurationBuilder = functools.partial(
+    PolarisationConfiguration,
+    polarisations_id="all",
+    corr_type=("XX", "XY", "YY", "YX"),
+)
 
-    By default, BeamConfigurationBuilder will build an BeamConfiguration
-
-    for low observation command.
-    """
-
-    def __init__(self) -> "BeamConfigurationBuilder":
-        self.beam_id = None
-        self.function = None
-        self.search_beam_id = None
-        self.timing_beam_id = None
-        self.vlbi_beam_id = None
-
-    def set_beam_id(self, beam_id: str) -> "BeamConfigurationBuilder":
-        """
-        Set the beam id
-        :param beam_id: Beam id
-        """
-        self.beam_id = beam_id
-        return self
-
-    def set_function(self, function: str) -> "BeamConfigurationBuilder":
-        """
-        Set the function
-        :param function: Function
-        """
-        self.function = function
-        return self
-
-    def set_search_beam_id(
-        self, search_beam_id: int
-    ) -> "BeamConfigurationBuilder":
-        """
-        Set the search beam id
-        :param search_beam_id: Search beam id
-        """
-        self.search_beam_id = search_beam_id
-        return self
-
-    def set_timing_beam_id(
-        self, timing_beam_id: int
-    ) -> "BeamConfigurationBuilder":
-        """
-        Set the timing beam id
-        :param timing_beam_id: Timing beam id
-        """
-        self.timing_beam_id = timing_beam_id
-        return self
-
-    def set_vlbi_beam_id(
-        self, vlbi_beam_id: int
-    ) -> "BeamConfigurationBuilder":
-        """
-        Set the vlbi beam id
-        :param vlbi_beam_id: Vlbi beam id
-        """
-        self.vlbi_beam_id = vlbi_beam_id
-        return self
-
-    def build(self) -> BeamConfiguration:
-        """
-        Build or create CDM BeamConfiguration object
-        :return: CDM BeamConfiguration object
-        """
-        return BeamConfiguration(
-            beam_id=self.beam_id,
-            function=self.function,
-            search_beam_id=self.search_beam_id,
-            timing_beam_id=self.timing_beam_id,
-            vlbi_beam_id=self.vlbi_beam_id,
-        )
+PhaseDirBuilder = functools.partial(
+    PhaseDir,
+    ra=(123, 0.1),
+    dec=(123, 0.1),
+    reference_time="...",
+    reference_frame="ICRF3",
+)
 
 
-class ChannelConfigurationBuilder:
-    """
-    ChannelConfigurationBuilder is a test data builder for CDM ChannelConfiguration objects.
-
-    By default, ChannelConfigurationBuilder will build an ChannelConfiguration
-
-    for low observation command.
-    """
-
-    def __init__(self) -> "ChannelConfigurationBuilder":
-        self.channels_id = None
-        self.spectral_windows = None
-
-    def set_channels_id(
-        self, channels_id: str
-    ) -> "ChannelConfigurationBuilder":
-        """
-        Set the channels id
-        :param channels_id: Channels id
-        """
-        self.channels_id = channels_id
-        return self
-
-    def set_spectral_windows(
-        self, spectral_windows: list
-    ) -> "ChannelConfigurationBuilder":
-        """
-        Set the spectral windows
-        :param spectral_windows: Spectral windows
-        """
-        self.spectral_windows = spectral_windows
-        return self
-
-    def build(self) -> ChannelConfiguration:
-        """
-        Build or create CDM ChannelConfiguration object
-        :return: CDM ChannelConfiguration object
-        """
-        return ChannelConfiguration(
-            channels_id=self.channels_id,
-            spectral_windows=self.spectral_windows,
-        )
-
-
-class PolarisationConfigurationBuilder:
-    """
-    PolarisationConfigurationBuilder is a test data builder for CDM PolarisationConfiguration objects.
-
-    By default, PolarisationConfigurationBuilder will build an PolarisationConfiguration
-
-    for low observation command.
-    """
-
-    def __init__(self) -> "PolarisationConfigurationBuilder":
-        self.polarisations_id = None
-        self.corr_type = None
-
-    def set_polarisations_id(
-        self, polarisations_id: str
-    ) -> "PolarisationConfigurationBuilder":
-        """
-        Set the polarisations id
-        :param polarisations_id: Polarisations id
-        """
-        self.polarisations_id = polarisations_id
-        return self
-
-    def set_corr_type(
-        self, corr_type: list
-    ) -> "PolarisationConfigurationBuilder":
-        """
-        Set the correlation type
-        :param corr_type: Correlation type
-        """
-        self.corr_type = corr_type
-        return self
-
-    def build(self) -> PolarisationConfiguration:
-        """
-        Build or create CDM PolarisationConfiguration object
-        :return: CDM PolarisationConfiguration object
-        """
-        return PolarisationConfiguration(
-            polarisations_id=self.polarisations_id, corr_type=self.corr_type
-        )
-
-
-class PhaseDirBuilder:
-    """
-    PhaseDirBuilder is a test data builder for CDM PhaseDir objects.
-
-    By default, PhaseDirBuilder will build an PhaseDir
-
-    for low observation command.
-    """
-
-    def __init__(self) -> "PhaseDirBuilder":
-        self.ra = None
-        self.dec = None
-        self.reference_time = None
-        self.reference_frame = None
-
-    def set_ra(self, ra: list) -> "PhaseDirBuilder":
-        """
-        Set the right ascension
-        :param ra: Right ascension
-        """
-        self.ra = ra
-        return self
-
-    def set_dec(self, dec: list) -> "PhaseDirBuilder":
-        """
-        Set the declination
-        :param dec: Declination
-        """
-        self.dec = dec
-        return self
-
-    def set_reference_time(self, reference_time: str) -> "PhaseDirBuilder":
-        """
-        Set the reference time
-        :param reference_time: Reference time
-        """
-        self.reference_time = reference_time
-        return self
-
-    def set_reference_frame(self, reference_frame: str) -> "PhaseDirBuilder":
-        """
-        Set the reference frame
-        :param reference_frame: Reference frame
-        """
-        self.reference_frame = reference_frame
-        return self
-
-    def build(self) -> PhaseDir:
-        """
-        Build or create CDM PhaseDir object
-        :return: CDM PhaseDir object
-        """
-        return PhaseDir(
-            ra=self.ra,
-            dec=self.dec,
-            reference_time=self.reference_time,
-            reference_frame=self.reference_frame,
-        )
-
-
-class FieldConfigurationBuilder:
-    """
-    FieldConfigurationBuilder is a test data builder for CDM FieldConfiguration objects.
-
-    By default, FieldConfigurationBuilder will build an FieldConfiguration
-
-    for low observation command.
-    """
-
-    def __init__(self) -> "FieldConfigurationBuilder":
-        self.field_id = None
-        self.pointing_fqdn = None
-        self.phase_dir = None
-
-    def set_field_id(self, field_id: str) -> "FieldConfigurationBuilder":
-        """
-        Set the field id
-        :param field_id: Field id
-        """
-        self.field_id = field_id
-        return self
-
-    def set_pointing_fqdn(
-        self, pointing_fqdn: str
-    ) -> "FieldConfigurationBuilder":
-        """
-        Set the pointing fqdn
-        :param pointing_fqdn: Pointing fqdn
-        """
-        self.pointing_fqdn = pointing_fqdn
-        return self
-
-    def set_phase_dir(
-        self, phase_dir: PhaseDir
-    ) -> "FieldConfigurationBuilder":
-        """
-        Set the phase dir
-        :param phase_dir: Phase dir
-        """
-        self.phase_dir = phase_dir
-        return self
-
-    def build(self) -> FieldConfiguration:
-        """
-        Build or create CDM FieldConfiguration object
-        :return: CDM FieldConfiguration object
-        """
-        return FieldConfiguration(
-            field_id=self.field_id,
-            pointing_fqdn=self.pointing_fqdn,
-            phase_dir=self.phase_dir,
-        )
-
+FieldConfigurationBuilder = functools.partial(
+    FieldConfiguration,
+    field_id="field_a",
+    pointing_fqdn="low-tmc/telstate/0/pointing",
+    phase_dir=PhaseDirBuilder(),
+)
 
 EBScanTypeBuilder = functools.partial(
     EBScanType,
@@ -453,109 +167,22 @@ EBScanTypeBeamBuilder = functools.partial(
 )
 
 
-class ExecutionBlockConfigurationBuilder:
-    """
-    ExecutionBlockConfigurationBuilder is a test data builder for CDM ExecutionBlockConfiguration objects.
+ExecutionBlockConfigurationBuilder = functools.partial(
+    ExecutionBlockConfiguration,
+    eb_id="eb-mvp01-20200325-00001",
+    max_length=3600,
+    beams=(BeamConfigurationBuilder(),),
+    channels=(ChannelConfigurationBuilder(),),
+    polarisations=(PolarisationConfigurationBuilder(),),
+    fields=(FieldConfigurationBuilder(),),
+    scan_types=(EBScanTypeBuilder(),),
+)
 
-    By default, ExecutionBlockConfigurationBuilder will build an ExecutionBlockConfiguration
-
-    for low observation command.
-    """
-
-    def __init__(self) -> "ExecutionBlockConfigurationBuilder":
-        self.eb_id = None
-        self.max_length = None
-        self.context = {}
-        self.beams = None
-        self.channels = None
-        self.polarisations = None
-        self.fields = None
-        self.scan_types = None
-
-    def set_eb_id(self, eb_id: str) -> "ExecutionBlockConfigurationBuilder":
-        """
-        Set the eb id
-        :param eb_id: EB id
-        """
-        self.eb_id = eb_id
-        return self
-
-    def set_max_length(self, max_length: float):
-        """
-        Set the max length
-        :param max_length: Max length
-        """
-        self.max_length = max_length
-        return self
-
-    def set_context(
-        self, context: dict
-    ) -> "ExecutionBlockConfigurationBuilder":
-        """
-        Set the context
-        :param context: Context
-        """
-        self.context = context
-        return self
-
-    def set_beams(self, beams: list) -> "ExecutionBlockConfigurationBuilder":
-        """
-        Set the beams
-        :param beams: Beams
-        """
-        self.beams = beams
-        return self
-
-    def set_channels(
-        self, channels: list
-    ) -> "ExecutionBlockConfigurationBuilder":
-        """
-        Set the channels
-        :param channels: Channels
-        """
-        self.channels = channels
-        return self
-
-    def set_polarisations(
-        self, polarisations: list
-    ) -> "ExecutionBlockConfigurationBuilder":
-        """
-        Set the polarisations
-        :param polarisations: Polarisations
-        """
-        self.polarisations = polarisations
-        return self
-
-    def set_fields(self, fields: list) -> "ExecutionBlockConfigurationBuilder":
-        """
-        Set the fields
-        :param fields: Fields
-        """
-        self.fields = fields
-        return self
-
-    def set_scan_types(
-        self, scan_types: list
-    ) -> "ExecutionBlockConfigurationBuilder":
-        """
-        Set the scan types
-        :param scan_types: Scan types
-        """
-        self.scan_types = scan_types
-        return self
-
-    def build(self) -> ExecutionBlockConfiguration:
-        """
-        Build or create CDM ExecutionBlockConfiguration object
-        :return: CDM ExecutionBlockConfiguration object
-        """
-        return ExecutionBlockConfiguration(
-            eb_id=self.eb_id,
-            max_length=self.max_length,
-            context=self.context,
-            beams=self.beams,
-            channels=self.channels,
-            polarisations=self.polarisations,
-            fields=self.fields,
-            scan_types=self.scan_types,
-        )
+SDPConfigurationBuilder = functools.partial(
+    SDPConfiguration,
+    eb_id="sbi-mvp01-20200325-00001",
+    max_length=100.0,
+    scan_types=(ScanTypeBuilder(),),
+    processing_blocks=(ProcessingBlockConfigurationBuilder(),),
+    execution_block=ExecutionBlockConfigurationBuilder(),
+)
