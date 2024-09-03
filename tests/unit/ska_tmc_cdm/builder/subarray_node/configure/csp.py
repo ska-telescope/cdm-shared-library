17abebf7719222b1c9762bcb31ab2ebcab5bc200
Brendan McCollam <brendan.mccollam@stfc.ac.uk>
2024-09-03 09:55:11 +0100

    BTN-2456: [WIP] test_csp

--- tests/unit/ska_tmc_cdm/builder/subarray_node/configure/csp.py
+++ tests/unit/ska_tmc_cdm/builder/subarray_node/configure/csp.py
@@ -1,7 +1,7 @@
 import functools
 from typing import List, Tuple
 
-from ska_tmc_cdm.messages.subarray_node.configure import core
+from ska_tmc_cdm.messages.subarray_node.configure.core import ReceiverBand
 from ska_tmc_cdm.messages.subarray_node.configure.csp import (
     BeamsConfiguration,
     CBFConfiguration,
@@ -21,11 +21,28 @@ from ska_tmc_cdm.messages.subarray_node.configure.csp import (
     VisStnBeamConfiguration,
 )
 
-FSPConfigurationBuilder = functools.partial(FSPConfiguration)
+FSPConfigurationBuilder = functools.partial(
+    FSPConfiguration,
+    fsp_id=1,
+    function_mode=FSPFunctionMode.CORR,
+    frequency_slice_id=1,
+    integration_factor=10,
+    zoom_factor=0,
+)
 
-SubarrayConfigurationBuilder = functools.partial(SubarrayConfiguration)
 
-CommonConfigurationBuilder = functools.partial(CommonConfiguration)
+SubarrayConfigurationBuilder = functools.partial(
+    SubarrayConfiguration, subarray_name="Test Subarray"
+)
+
+CommonConfigurationBuilder = functools.partial(
+    CommonConfiguration,
+    config_id="sbi-mvp01-20200325-00001-science_A",
+    frequency_band=ReceiverBand.BAND_1,
+    subarray_id=1,
+    band_5_tuning=(5.85, 7.25),
+)
+
 
 CBFConfigurationBuilder = functools.partial(CBFConfiguration)
 
@@ -37,7 +54,11 @@ LowCBFConfigurationBuilder = functools.partial(LowCBFConfiguration)
 
 StationConfigurationBuilder = functools.partial(StationConfiguration)
 
-StnBeamConfigurationBuilder = functools.partial(StnBeamConfiguration)
+StnBeamConfigurationBuilder = functools.partial(StnBeamConfiguration,
+            stn_beam_id=1¸
+            beam_id=1,
+            freq_ids=(400,),
+            delay_poly="tango/device/instance/delay",)
 
 VisFspConfigurationBuilder = functools.partial(VisFspConfiguration)
 
