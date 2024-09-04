904095c7f82580161de3c6ff40095cc3ea69c733
Brendan McCollam <brendan.mccollam@stfc.ac.uk>
2024-09-04 10:43:16 +0100

    BTN-2456: Remove superfluous tests

--- tests/unit/ska_tmc_cdm/messages/central_node/test_release_resources.py
+++ tests/unit/ska_tmc_cdm/messages/central_node/test_release_resources.py
@@ -27,36 +27,6 @@ def test_validation_applies_to_subarray_id(subarray_id: int, okay: bool):
         ReleaseResourcesRequestBuilder(subarray_id=subarray_id)
 
 
-def test_release_resources_request_has_interface_set_on_creation():
-    request = ReleaseResourcesRequestBuilder(interface=SCHEMA)
-    assert request.interface is not None
-
-
-@pytest.mark.parametrize(
-    "object1, object2, is_equal",
-    [
-        # Case where both objects are the same
-        (
-            ReleaseResourcesRequestBuilder(),
-            ReleaseResourcesRequestBuilder(),
-            True,
-        ),
-        # Case where objects differ by subarray_id
-        (
-            ReleaseResourcesRequestBuilder(subarray_id=1),
-            ReleaseResourcesRequestBuilder(subarray_id=2),
-            False,
-        ),
-    ],
-)
-def test_release_resources_request_eq_check(object1, object2, is_equal):
-    """
-    Verify that ReleaseResourcesRequest objects are equal or not equal based on their attributes.
-    """
-    assert (object1 == object2) == is_equal
-    assert object1 != object()
-
-
 def test_deallocate_resources_must_define_resources_if_not_releasing_all():
     """
     Verify that resource argument(s) must be set if the command is not a
@@ -64,14 +34,3 @@ def test_deallocate_resources_must_define_resources_if_not_releasing_all():
     """
     with pytest.raises(ValueError):
         ReleaseResourcesRequestBuilder(release_all=False, dish_allocation=None)
-
-
-def test_deallocate_resources_enforces_boolean_release_all_argument():
-    """
-    Verify that the boolean release_all_mid argument is required.
-    """
-    with pytest.raises(ValueError):
-        ReleaseResourcesRequestBuilder(release_all=1, dish_allocation=None)
-
-    with pytest.raises(ValueError):
-        ReleaseResourcesRequestBuilder(release_all=1)
