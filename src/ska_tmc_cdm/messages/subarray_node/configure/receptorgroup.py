ad60471246c3e117f742b120afd6e8a1702b0fcb
Stewart Williams <3161869-bravostuzero@users.noreply.gitlab.com>
2024-10-11 14:38:50 +0100

    BTN-2460: refactor receptor group classes
    Renames various classes related to receptors groups, pointing,
    projections, and tracking to indicate they are not just intended
    for holography. Also refactors their implementation for clearer scope.

---
+++ src/ska_tmc_cdm/messages/subarray_node/configure/receptorgroup.py
@@ -0,0 +1,96 @@
+from __future__ import annotations
+
+from enum import Enum
+from typing import Literal, Optional, Union
+
+from pydantic import Discriminator, Field
+from typing_extensions import Annotated
+
+from ska_tmc_cdm import CdmObject
+from ska_tmc_cdm.messages.skydirection import SkyDirection
+
+
+class TrajectoryType(str, Enum):
+    """
+    Holography Scan Pattern
+    """
+
+    FIXED = "fixed"
+    MOSAIC = "mosaic"
+    SPIRAL = "spiral"
+    RASTER = "raster"
+    CONSTANT_VELOCITY = "constant-velocity"
+    TABLE = "table"
+    HYPOTROCHOID = "hypotrochoid"
+
+
+class MosaicTrajectory(CdmObject):
+    name: Literal[TrajectoryType.MOSAIC] = TrajectoryType.MOSAIC
+    attrs: MosaicTrajectory.Attrs
+
+    class Attrs(CdmObject):
+        x_offsets: list[float]
+        y_offsets: list[float]
+
+
+class TableTrajectory(CdmObject):
+    name: Literal[TrajectoryType.TABLE] = TrajectoryType.TABLE
+    attrs: TableTrajectory.Attrs
+
+    class Attrs(CdmObject):
+        x: float
+        y: float
+        t: list[float] = Field(default_factory=list)
+
+
+class FixedTrajectoryConfig(CdmObject):
+    name: Literal[TrajectoryType.FIXED] = TrajectoryType.FIXED
+
+    class Attrs(CdmObject):
+        x: float
+        y: float
+        t: list[float] = Field(default_factory=list)
+
+
+Trajectory = Annotated[
+    Union[MosaicTrajectory, TableTrajectory, FixedTrajectoryConfig],
+    Discriminator("name"),
+]
+
+
+class ProjectionType(str, Enum):
+    """Projection Names"""
+
+    SIN = "SIN"
+    TAN = "TAN"
+    ARC = "ARC"
+    STG = "STG"
+    CAR = "CAR"
+    SSN = "SSN"
+
+
+class ProjectionAlignment(str, Enum):
+    """Projection Alignment"""
+
+    ICRS = "ICRS"
+    ALTAZ = "AltAz"
+
+
+class Projection(CdmObject):
+    """
+    Projection Config
+    """
+
+    name: ProjectionType = ProjectionType.SIN
+    alignment: ProjectionAlignment = ProjectionAlignment.ICRS
+
+
+class ReceptorGroup(CdmObject):
+    """
+    Receptor Group defines a set of receptors plus the sky direction and
+    tracking/mapping strategy for target tracking."""
+
+    receptors: list[str] = Field(default_factory=list)
+    field: SkyDirection
+    trajectory: Optional[Trajectory] = None
+    projection: Optional[Projection] = None
