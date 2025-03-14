5163268043d21ba69d75d678be7a1603f75e78b5
Stewart Williams <3161869-bravostuzero@users.noreply.gitlab.com>
2025-03-14 10:53:43 +0100

    BTN-2635: update to ska-telmodel 1.20.1
    Updates dependencies to use ska-telmodel 1.20.1, including
    ska-tmc-configure v4.2.

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,3 +9,15 @@ HERE = Path(__file__).parent.resolve()
 def local_tmdata(monkeypatch):
     fixture_tmdata = str(HERE / "fixtures/tmdata")
     monkeypatch.setenv("SKA_TELMODEL_SOURCES", fixture_tmdata)
+
+
+@pytest.fixture(autouse=True)
+def allow_strict_telmodel_validation(monkeypatch):
+    """
+    Allow strictness=2 for CDM unit tests.
+
+    ska-telmodel v1.20 changed behaviour so that strictness=2 is downgraded to
+    strictness=1 unless certain recognised environment variables are set. This
+    fixture sets one of those environment variables.
+    """
+    monkeypatch.setenv("SKA_TELMODEL_ALLOW_STRICT_VALIDATION", "1")
